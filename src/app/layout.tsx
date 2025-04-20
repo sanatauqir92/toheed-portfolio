1f9d4792eef495aae0a17fa94d61f708d59bf280
sanatauqir92 <sanatauqir92@gmail.com>
2025-04-19 18:11:01 -0700

    in-progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/app/components/Header";
 export const metadata: Metadata = {
   title: "Toheed Chaudhry Portfolio",
   description: "My professional journey through film",
+  viewport: { width: 'device-width', initialScale: 1 },
 };
 
 export default function RootLayout({
@@ -17,7 +18,7 @@ export default function RootLayout({
       <body>
         <div className="bg-white min-h-screen grid grid-rows-[auto_1fr_auto] w-full">
           <Header />
-          <main className="text-black text-xl w-2/3 mx-auto mt-6 overflow-hidden">
+          <main className="text-black text-xl w-3/4 lg:w-2/3 mx-auto mt-6 overflow-hidden">
             {children}
           </main>
         </div>
