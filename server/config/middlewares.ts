6014101964d0c1109cdcb08ddaca1892d8fcc8c6
sanatauqir92 <sanatauqir92@gmail.com>
2025-06-02 16:02:56 -0700

    middleware updates

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,6 +1,40 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.PUBLIC_URL || 'toheed-portfolio-production.up.railway.app',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.PUBLIC_URL || 'toheed-portfolio-production.up.railway.app',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://dreamy-daifuku-3ed50f.netlify.app',
+        process.env.PUBLIC_URL,
+      ],
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
