580880105e8d3028e204785de575b3694407b7ce
sanatauqir92 <sanatauqir92@gmail.com>
2025-08-26 15:03:59 -0700

    homepage video and resized images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,19 @@
 import Grid from './components/Grid';
 
 export default function Home() {
-  return <Grid />;
+  return (
+    <>
+      <div className="relative w-full aspect-video mb-4">
+        <iframe 
+          src="https://www.youtube.com/embed/OId2DxZl9yk?si=PwIqbd54MINGyMHr" 
+          title="YouTube video player" 
+          referrerPolicy="strict-origin-when-cross-origin" 
+          className="absolute top-0 left-0 w-full h-full"
+          allow="autoplay; encrypted-media" 
+          allowFullScreen 
+        />
+      </div>
+      <Grid/>
+    </>
+  )
 }
