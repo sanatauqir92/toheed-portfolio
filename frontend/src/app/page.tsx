b5be4ff9043c3a7221cc530393b41184a6165298
sanatauqir92 <sanatauqir92@gmail.com>
2025-12-31 17:44:02 -0800

    add video to homepage and new images for projects

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,27 @@ import Grid from './components/Grid';
 export default function Home() {
   return (
     <>
-      <div className="relative w-full aspect-video mb-4">
-        <iframe
-          src="https://www.youtube.com/embed/9Y66_htOQ3g?si=HJA18r6qmNdXsVsz"
-          title="YouTube video player"
-          referrerPolicy="strict-origin-when-cross-origin"
-          className="absolute top-0 left-0 w-full h-full"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <div className="relative w-full aspect-video">
+          <iframe
+            src="https://www.youtube.com/embed/9Y66_htOQ3g?si=HJA18r6qmNdXsVsz"
+            title="YouTube video player"
+            referrerPolicy="strict-origin-when-cross-origin"
+            className="absolute top-0 left-0 w-full h-full"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
+        <div className="relative w-full aspect-video">
+          <iframe
+            src="https://www.youtube.com/embed/OId2DxZl9yk?si=yoJBNJp1sFkjNmIt"
+            title="YouTube video player"
+            referrerPolicy="strict-origin-when-cross-origin"
+            className="absolute top-0 left-0 w-full h-full"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+          />
+        </div>
       </div>
       <Grid />
     </>
