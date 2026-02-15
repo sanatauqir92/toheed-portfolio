cccc4c7234b06b3a2a8b8500e5d16e814d90a1fe
sanatauqir92 <sanatauqir92@gmail.com>
2026-02-16 08:02:17 +1000

    centering text

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
   return (
     <header className="w-full h-48 lg:h-56 bg-[#211814]" style={{ colorScheme: 'dark' }}>
       <div className="mx-auto mt-12 lg:w-2/3 text-white">
-        <div className="mb-4">
-          <p className="text-3xl lg:text-5xl font-bold justify-self-center">
+        <div className="mb-4 text-center">
+          <p className="text-3xl lg:text-5xl font-bold">
             Toheed Chaudhry
           </p>
-          <p className="text-xl lg:text-2xl font-bold justify-self-center">
+          <p className="text-xl lg:text-2xl font-bold">
             Editor | DIT | 2nd AC
           </p>
         </div>
